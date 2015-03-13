8eb88c4c7918930412734b79703cab195de34d3b
Bryan Reilly <94bryanr@gmail.com>
2015-03-13 13:48:28 -0600

    Can see friends who have the app

--- Project/Partypush/src/main/java/csu/bryanreilly/partypush/UI/Main/Friends/FriendPickerActivity.java
+++ Project/Partypush/src/main/java/csu/bryanreilly/partypush/UI/Main/Friends/FriendPickerActivity.java
@@ -4,6 +4,13 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.facebook.HttpMethod;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+
 import csu.bryanreilly.partypush.R;
 import csu.bryanreilly.partypush.UserData.AccountManager;
 
@@ -12,6 +19,21 @@ public class FriendPickerActivity extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_picker);
+        final TextView text = (TextView)findViewById(R.id.textViewFriends);
+
+        //TODO: Make this data visible
+        /* make the API call */
+        new Request(
+                Session.getActiveSession(),
+                "/me/friends",
+                null,
+                HttpMethod.GET,
+                new Request.Callback() {
+                    public void onCompleted(Response response) {
+                        text.setText(response.toString());
+                    }
+                }
+        ).executeAsync();
     }
 
     public void addButtonClick(View view) {
